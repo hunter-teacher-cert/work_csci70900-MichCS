6f2b55e6b063be0e67506a59181b7dcd3e78a8ee
Michelle B <86215527+MichCS@users.noreply.github.com>
2021-07-01 13:23:52 +0000

    added 1 Date.java

---
+++ pre/1/Date.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+  public static void main(String[] args) {
+
+      String day = "Monday";
+      String month = "June";
+      int date = 21;
+      int year = 2021;
+
+    System.out.println("American format:");
+      System.out.println(day + ", " + month + " " + date + ", " + year);
+    System.out.println("European format:");
+      System.out.println(day + " " + date + " " + month + " " + year);
+
+
+  }
+}
