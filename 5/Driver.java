facd2ef9679d8ead991acf5784bd205db8cd4091
Michelle B <86215527+MichCS@users.noreply.github.com>
2021-07-07 14:31:39 +0000

    tested method and main, added comments 5 Driver.java

--- 5/Driver.java
+++ 5/Driver.java
@@ -5,9 +5,7 @@
 Your driver file should contain code to (in suggested development order)
 
 
-print all of the above (See multiple ways to do this? Try all -- and document in comments!)
 
-test for equality of each of the Time instances above. Print results.
 add two Time objects and print results
 add two Time objects and save the result in another Time object
 Your Time class definition file should contain code to
@@ -47,6 +45,31 @@ public class Driver {
     printTime(t);
     
 
+
+    //test for equality of each of the Time instances above. Print results.
+
+    checkTime(t, var); //same time (t and var are same)
+
+    t = new Time(12,50,30.2); //change t
+    checkTime(t, var); //different time (t and var are different)
+    checkTime(t, var); //different time (t and var are different)
+70    checkTime(t, var); //different time (t and var are different)
+
+    //add two Time objects and print results
+    Time t1 = new Time(9,20,25.1); //start time
+    Time t2 = new Time(9,29,34.2); //finished time
+
+    //add two Time objects and save the result in another Time object
+    Time endTime = Time.add(t1, t2); //added start and finished time
+
+    System.out.print("t1: ");
+    printTime(t1);
+    System.out.print("t2: ");
+    printTime(t2);
+    System.out.print("t1 + t2: ");
+    printTime(endTime);
+
+
   }//end main()
 
 //Method to print time
@@ -54,5 +77,30 @@ public class Driver {
     System.out.printf("%02d:%02d:%04.1f\n",
         t.hour, t.minute, t.second);
   }//end of printTime Method
+
+
+//Testing for equality (attributes) of the Time instance
+  public static void checkTime(Time t, Time var){
+    System.out.print("Time t is: " );
+    printTime(t);
+    System.out.print(" and time var is: " );
+    printTime(var);
+    if (t.hour == var.hour && t.minute == var.minute && t.second == var.second){
+      System.out.println("Time t and time var are the same.");
+    }
+    else{
+      System.out.println("Time t and time var are different.");
+    }
+  }
+    
+    
+  //Adding two Time objects   
+  /*public static Time add(Time t1, Time t2) {
+    Time sum = new Time();
+    sum.hour = t1.hour + t2.hour;
+    sum.minute = t1.minute + t2.minute;
+    sum.second = t1.second + t2.second;
+    return sum;
+  }//end of Time Method*/
 
 }//end class
